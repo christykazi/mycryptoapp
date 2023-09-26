c41d8eb48ed5784035a66b520461b0528e92de0e
christykazi <christyagbai4178@gmail.com>
2023-09-26 23:57:06 +0100

    successfull debuging

--- src/components/screens/CoinDetailedScreens/index.jsx
+++ src/components/screens/CoinDetailedScreens/index.jsx
@@ -4,11 +4,11 @@ import Coin from "../../../../assets/data/crytpo.json";
 import CoinDetailedHeader from "./comp/CoinDetailedHeader";
 import { AntDesign } from "@expo/vector-icons";
 import styles from "./styles";
-import {
-  ChartDot,
-  ChartPath,
-  ChartPathProvider,
-} from "@rainbow-me/animated-charts";
+// import {
+//   ChartDot,
+//   ChartPath,
+//   ChartPathProvider,
+// } from "@rainbow-me/animated-charts";
 
 const CoinDetailedScreens = () => {
   const {
@@ -26,12 +26,12 @@ const CoinDetailedScreens = () => {
   const percentageColor =
     price_change_percentage_24h < 0 ? "#ea3943" : "#16c784";
 
-  const screenWidth = Dimensions.get("window").width;
+ /*  const screenWidth = Dimensions.get("window").width; */
 
   return (
     <View style={{ paddingHorizontal: 10 }}>
-      <ChartPathProvider
-        data={{
+     {/*  <ChartPathProvider */}
+       {/*  data={{
           points: prices.map(([x, y]) => ({ x, y})),
           smoothingsStrategy: "bezier",
         }}
@@ -41,7 +41,7 @@ const CoinDetailedScreens = () => {
           name={name}
           symbol={symbol}
           marketCapRank={market_cap_rank}
-        />
+        /> */}
         <View style={styles.priceContainer}>
           <View>
             <Text style={styles.name}>{name}</Text>
@@ -68,13 +68,13 @@ const CoinDetailedScreens = () => {
           </View>
         </View>
 
-        <ChartPath
+      {/*   <ChartPath
           height={screenWidth / 2}
           stroke="yellow"
           width={screenWidth}
         />
         <ChartDot style={{ backgroundColor: "blue" }} />
-      </ChartPathProvider>
+      </ChartPathProvider> */}
     </View>
   );
 };
