be1d98ae98c5d0283ab6ad75d84d88e51eb40ae9
christykazi <christyagbai4178@gmail.com>
2023-10-01 23:45:33 +0100

    errors solving

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,17 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: ['react-native-reanimated/plugin',],
   };
 };
+
+// module.exports = {
+//   presets: [
+//     ... // don't add it here :)
+//   ],
+//   plugins: [
+//     ...
+//     'react-native-reanimated/plugin',
+//   ],
+// };
+
